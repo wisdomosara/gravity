2a6e97c0461550f4c6000edc63bd088c5f218f1c
wisdomosara <wisdomosara@gmail.com>
2021-01-31 17:04:52 +0100

    added

---
+++ components/Layout.tsx
@@ -0,0 +1,128 @@
+import React, { ReactNode, useState } from "react";
+// import Link from 'next/link'
+import Head from "next/head";
+import styles from "./Header/Header.module.scss";
+import { motion } from "framer-motion";
+
+type Props = {
+  children?: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title = "This is the default title" }: Props) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const style = {
+    borderRight: "3px solid black",
+    borderTop: "1px solid black",
+    borderBottom: "3px solid black",
+    borderLeft: "1px solid black",
+    borderRadius: "8px",
+  };
+
+  return (
+    <div className="xl:px-32 md:px-10 lg:px-20 px-5 overflow-hidden">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="preload"
+          href="/fonts/sf/sfDisplay.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <header className="w-full">
+        <nav
+          className="flex justify-between items-center py-10"
+          style={{ zIndex: 3 }}
+        >
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.1,
+                },
+              },
+            }}
+          >
+            <h2 className="text-2xl md:text-4xl font-bold">Gravity</h2>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.2,
+                },
+              },
+            }}
+          >
+            <div className="hidden md:block">
+              <ul className="flex items-center">
+                <li className="pl-10 font-bold text-xl hover:opacity-50">
+                  Features
+                </li>
+                <li className="pl-10 font-bold text-xl hover:opacity-50">
+                  Pricing
+                </li>
+                <li className="pl-10 font-bold text-xl hover:opacity-50">
+                  Why Gravity
+                </li>
+                <li
+                  className="pl-10 font-bold text-xl hover:opacity-50"
+                  style={{ color: "#71A89E" }}
+                >
+                  Sign In
+                </li>
+              </ul>
+            </div>
+            <div className="relative md:hidden">
+              <button
+                className="flex items-center w-24  justify-center py-2 hover:outline-none"
+                style={style}
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                <img src="/images/menu.svg" width="11px" height="11px" alt="" />{" "}
+                <p className="ml-4 font-bold text-xs">Menu</p>
+              </button>
+              <div className={showMenu ? styles.absolute : styles.noAbsolute}>
+                <ul>
+                  <li className="text-center py-2 hover:bg-white">Features</li>
+                  <li className="text-center py-2 hover:bg-white">Pricing</li>
+                  <li className="text-center py-2 hover:bg-white">
+                    Why Gravity
+                  </li>
+                  <li
+                    className="text-center py-2 hover:bg-white"
+                    style={{ color: "#71A89E" }}
+                  >
+                    Sign In
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </motion.div>
+        </nav>
+      </header>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { motion } from "framer-motion";
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <section className="flex items-center flex-wrap">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        className="md:w-2/4 w-full order-2 md:order-1"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            x: -100,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            x: 0,
+            opacity: 1,
+            transition: {
+              delay: 0.3,
+            },
+          },
+        }}
+      >
+        <div>
+          <h2 className="xl:text-5xl lg:text-2xl md:text-4xl text-3xl font-normal mt-10 md:mt-0  mb-3 md:mb-5 xl:mb-10">
+            Simple time tracking.
+          </h2>
+          <h2 className="xl:text-5xl md:text-4xl text-3xl font-normal">
+            {" "}
+            Powerful reporting.
+          </h2>
+          <p className="text-base  md:text-xl xl:text-2xl font-normal md:pr-16 mb-10 xl:mt-10 mt-5">
+            Turn your team on to productivity with Gravity the time tracker.
+          </p>
+          <button className="rounded-sm btn py-2 md:py-5 text-white mb-24 md:mb-0">
+            Request early access
+          </button>
+        </div>
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        className="md:w-2/4 w-full order-1 md:order-2"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            x: 100,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            x: 0,
+            opacity: 1,
+            transition: {
+              delay: 0.5,
+            },
+          },
+        }}
+      >
+        <div>
+          <img
+            src="images/illustration.png"
+            width="100%"
+            height="100%"
+            alt=""
+            className="-mt-28"
+          />
+        </div>
+      </motion.div>
+    </section>
+  </Layout>
+);
+
+export default IndexPage;
